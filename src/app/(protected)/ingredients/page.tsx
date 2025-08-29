85e16252cddd8fdd7e036ec5813979fb0053eac9
DariaZhur <dariazhur89@gmail.com>
2025-08-29 13:32:53 +0300

    add use client

--- src/app/(protected)/ingredients/page.tsx
+++ src/app/(protected)/ingredients/page.tsx
@@ -1,13 +1,15 @@
+"use client";
+
 import IngredientsTable from "@/components/UI/tables/ingredients";
 import IngredientForm from "@/forms/indredient.form";
 
 const IngredientsPage = () => {
-  return (
-    <div>
-      <IngredientForm />
-      <IngredientsTable />
-    </div>
-  );
+    return (
+        <div>
+            <IngredientForm />
+            <IngredientsTable />
+        </div>
+    );
 };
 
 export default IngredientsPage;
